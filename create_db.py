5220ff1834510f45c1d807a0fc59062a463b3a5b
Emile <133158597+Elie00000@users.noreply.github.com>
2024-03-18 08:14:32 +0000

    uirbui

--- create_db.py
+++ create_db.py
@@ -15,6 +15,6 @@ cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LAMB
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGNON', 'Nicolas', '456, Boulevard des Cerisiers, 69003 Lyon'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
 cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
-
+cur.execute("INSERT INTO livres (titre, auteur, disponible) VALUES (?, ?, ?)",('La nuit des temps', 'R.Barjavel', 'Oui'))
 connection.commit()
 connection.close()
